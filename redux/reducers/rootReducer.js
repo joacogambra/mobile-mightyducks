6f1102a45160e0da2a0cbb34f7fb54a174385a55
joacogambra <joaquingambra@gmail.com>
2022-12-05 15:54:55 -0300

    root reducer

---
+++ redux/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+import hotelsReducer from './hotelsReducer'
+import citiesReducer from "./citiesReducer";
+import citiesFilterReducer from "./citiesFilterReducer";
+import itineraryReducer from './itineraryReducer'
+import showReducer from './showReducer'
+import userReducer from './userReducer';
+import commentReducer from './commentReducer'
+import reactionReducer from './reactionReducer';
+
+
+const rootReducer = {
+    hotelsReducer, citiesReducer, citiesFilterReducer, itineraryReducer, showReducer, userReducer, commentReducer, reactionReducer
+}
+export default rootReducer
