d5fed864de17454a23835a7182c35e92b5e86e5f
joacogambra <joaquingambra@gmail.com>
2022-12-08 17:56:21 -0300

    navigation bottom

--- src/Navigations/Bottom.js
+++ src/Navigations/Bottom.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../Screens/Home'
 import SignUp from '../Screens/SignUp'
 import Cities from '../Screens/Cities'
+import Reactions from '../Screens/Reactions'
 
 const StackNav = createBottomTabNavigator();
 
@@ -14,6 +15,7 @@ export default function Bottom() {
     <StackNav.Screen name="Home" component={Home} />
     <StackNav.Screen name="SignUp" component={SignUp} />
     <StackNav.Screen name="Cities" component={Cities} />
+    <StackNav.Screen name="My Reactions" component={Reactions} />
   </StackNav.Navigator>
   )
 }
