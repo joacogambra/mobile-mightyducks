7b44863d2fc163eebd5e33f10d8ee7fa3f9ef254
joacogambra <joaquingambra@gmail.com>
2022-12-09 13:16:10 -0300

    component my profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,25 +1,36 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, Button } from 'react-native'
+import { useNavigation} from '@react-navigation/native'
+
 
 export default function MyProfile({ name, lastName, photo, age, email, _id }) {
+    let navigation = useNavigation()
 
     return (
         <View style={styles.containerView}>
-            <Text style={{ color: 'white', textAlign: 'center', fontWeight: "bold", fontSize: 42 }}>{name} {lastName}</Text>
-            <Image style={{ width: 310, height: '100%', margin: 10, resizeMode: 'cover' }} source={{ uri: photo }} />
-            <Text style={{ color: 'white', textAlign: 'center', fontWeight: "bold", fontSize: 32}}>Age: {age}</Text>
-            <Text style={{ color: 'white', textAlign: 'center', fontWeight: "bold", fontSize: 32}}>Email: {email}</Text>
+            <Text style={{ color: 'white', textAlign: 'center', fontWeight: "bold", fontSize: 38 }}>{name} {lastName}</Text>
+            <Image style={{ width: 310,height:280, margin: 10, resizeMode: 'cover' }} source={{ uri: photo }} />
+            <Text style={{ color: 'white', textAlign: 'center', fontWeight: "bold", fontSize: 28}}>Age: {age}</Text>
+            <Text style={{ color: 'white', textAlign: 'center', fontWeight: "bold", fontSize: 28}}>Email: {email}</Text>
+            <View style={{marginTop:23}}>
+                <Button title='My Reactions' onPress={() => navigation.navigate('Reactions')}></Button>
+            </View>  
+            <View style={{marginTop:23}}>
+                <Button title='Edit'></Button>
+            </View>           
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     containerView: {
+        display:'flex',
+        alignItems:'center',
+        flex:1,
         width: '100%',
         height: '100%',
-        alignSelf: 'center',
         borderWidth: 1,
-        backgroundColor: 'rgba(118, 117, 117, 0.65)',
+        backgroundColor: 'rgba(118, 117, 117, 0.55)',
         borderRadius: 20,
         shadowColor: 'rgba(109, 109, 109, 1)',
         borderBottomWidth: 0,
@@ -27,6 +38,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 2,
         elevation: 1,
-        margin: 5,  
+        marginTop:34,
+        textShadowColor: '#303030' 
     }
 })
